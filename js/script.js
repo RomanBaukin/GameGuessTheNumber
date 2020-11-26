494d2086bbf0efb6eb9332260c32c2137e2077b9
Roman Baukin <rbaukin1984@gmail.com>
2020-11-26 16:39:29 +0400

    Урок №6 обязательное задание

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
 'use strict';
 
-// let randomNumberGeneration = function () {
-//   return Math.ceil(Math.random() * 100);
-// };
 let guessNumber = function () {
   let randomNumber = Math.ceil(Math.random() * 100);
 
   let checkAnswer = function () {
     let insertNumber = prompt('Угадай число от 1 до 100');
-
-    // console.log(randomNumber);
-    // console.log(typeof insertNumber);
 
     if (+insertNumber === randomNumber) {
       alert(`Поздравляю, Вы угадали! Загаданное число ${randomNumber}`);
