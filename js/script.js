72f0cb6802388dcb1d914adcf86d1c137492323c
Roman Baukin <rbaukin1984@gmail.com>
2020-11-26 16:34:39 +0400

    Урок №6 усложненное задание

--- js/script.js
+++ js/script.js
@@ -1,32 +1,51 @@
 'use strict';
 
-// let randomNumberGeneration = function () {
-//   return Math.ceil(Math.random() * 100);
-// };
 let guessNumber = function () {
   let randomNumber = Math.ceil(Math.random() * 100);
+  let attemptCounter = 10;
 
   let checkAnswer = function () {
     let insertNumber = prompt('Угадай число от 1 до 100');
 
-    // console.log(randomNumber);
-    // console.log(typeof insertNumber);
+    attemptCounter--;
 
     if (+insertNumber === randomNumber) {
       alert(`Поздравляю, Вы угадали! Загаданное число ${randomNumber}`);
+      if (confirm('Хотели бы сыграть ещё?')) {
+        guessNumber();
+      } else {
+        alert('Игра окончена');
+      }
     } else if (insertNumber === null) {
       alert('Игра окончена');
     } else if (insertNumber < randomNumber) {
-      alert('Загаданное число больше');
-      checkAnswer();
+      if (attemptCounter) {
+        alert(`Загаданное число больше. осталось попыток: ${attemptCounter}`);
+        checkAnswer();
+      } else {
+        if (confirm('Попытки закончились, хотите сыграть ещё?')) {
+          guessNumber();
+        } else {
+          alert('Игра окончена');
+        }
+      }
     } else if (insertNumber > randomNumber) {
-      alert('Загаданное число меньше');
-      checkAnswer();
+      if (attemptCounter) {
+        alert(`Загаданное число меньше. осталось попыток: ${attemptCounter}`);
+        checkAnswer();
+      } else {
+        if (confirm('Попытки закончились, хотите сыграть ещё?')) {
+          guessNumber();
+        } else {
+          alert('Игра окончена');
+        }
+      }
     } else {
       alert('Введи число!');
       checkAnswer();
     }
   };
+
   checkAnswer();
 };
 
